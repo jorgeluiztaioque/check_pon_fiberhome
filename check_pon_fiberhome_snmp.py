5db25cd09c6fd223359fbbd1978c6ce4e27f44c2
Jorge Luiz Taioque <jorgeluiztaioque@gmail.com>
2017-12-26 23:20:13 -0200

    desciption count onus and status by documentation

--- check_pon_fiberhome_snmp.py
+++ check_pon_fiberhome_snmp.py
@@ -18,6 +18,54 @@
 # check_command:	check_pon_fiberhome!10.10.10.1!1/8
 #-----------------------------------------------------------------------
 
+#PON NAME
+#--  1.3.6.1.4.1.5875.800.3.9.3.4.1.2
+#		-- 1.3.6.1.4.1.5875.800.3.9.3.4.1.2
+#		oltPonName OBJECT-TYPE
+#			SYNTAX OCTET STRING
+#			MAX-ACCESS read-only
+#			STATUS current
+#			DESCRIPTION
+#				"Column Description"
+#			::= { oltPonInfoEntry 2 }
+
+#PON STATUS
+#--  1.3.6.1.4.1.5875.800.3.9.3.4.1.5
+#		-- 1.3.6.1.4.1.5875.800.3.9.3.4.1.5
+#		oltPonOnlineStatus OBJECT-TYPE
+#			SYNTAX Integer32
+#			MAX-ACCESS read-only
+#			STATUS current
+#			DESCRIPTION
+#				"Column Description:
+#				1:occupied(1)
+#				2:empty(0)"
+#			::= { oltPonInfoEntry 5 }					
+
+#PON STATUS
+#--  1.3.6.1.4.1.5875.800.3.10.1.1.11
+#		-- 1.3.6.1.4.1.5875.800.3.10.1.1.11
+#		onuStatus OBJECT-TYPE
+#			SYNTAX Integer32 (0..1)
+#			MAX-ACCESS read-only
+#			STATUS current
+#			DESCRIPTION
+#				"Description:
+#				1:offonline/fiber cut/power failure(0)
+#				2:online(1)"
+#			::= { authOnuListEntry 11 }
+
+#ONU LIST BY PON
+#--  1.3.6.1.4.1.5875.800.3.10.1.1.3
+#		-- 1.3.6.1.4.1.5875.800.3.10.1.1.3
+#		authOnuListPon OBJECT-TYPE
+#			SYNTAX Integer32
+#			MAX-ACCESS read-only
+#			STATUS current
+#			DESCRIPTION
+#				"Column Description"
+#			::= { authOnuListEntry 3 }
+
 __author__ = 'Jorge Luiz Taioque'
 __version__= 0.1
 
