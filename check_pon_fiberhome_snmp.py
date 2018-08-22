979d8d01a504d43a96465119ab673ce09976eba8
Jorge Luiz Taioque <jorgeluiztaioque@gmail.com>
2018-08-21 22:30:43 -0300

    clean write comment of check_pon_fiberhome_snmp

--- check_pon_fiberhome_snmp.py
+++ check_pon_fiberhome_snmp.py
@@ -20,26 +20,18 @@
 __author__ = 'Jorge Luiz Taioque'
 __version__= 1.0
 
-import os
-import subprocess
 from easysnmp import Session
 import sys
 
 olt_ip = sys.argv[1]
 olt_port = sys.argv[2]
 
-
-#olt_ip = "10.110.50.1"
-#olt_port = "5"
 snmpSession = Session(hostname=olt_ip, community='adsl', version=2)
 
 mib_status = "1.3.6.1.4.1.5875.800.3.9.3.4.1.5"
 mib_onu_pon_num = "1.3.6.1.4.1.5875.800.3.9.3.4.1.12"
 
-#pon_name = subprocess.getoutput("snmpwalk -Os -c adsl -v 1 "+olt_ip+" 1.3.6.1.4.1.5875.800.3.9.3.4.1.2")
 pon_name_list = snmpSession.walk('1.3.6.1.4.1.5875.800.3.9.3.4.1.2')
-
-#pon_name_list =  pon_name.split()
 
 indice = []
 for j in range(1,17):
@@ -54,21 +46,15 @@ for i in indice:
 	mib_port = str(pon_name_list[i]).split()[3][41:][:-2]
 	readport = snmpSession.get(mib_status+"."+mib_port)
 	ponNumOnu = snmpSession.get(mib_onu_pon_num+"."+mib_port)
-	#readport = subprocess.getoutput("snmpwalk -Os -c adsl -v 1 "+olt_ip+" "+mib_status+"."+mib_port+"")
+
 	status_pon = str(readport).split()[1][-2:-1]
 	numOnu = str(ponNumOnu).split()[1][7:-1]
 	result.update({key:[status_pon,numOnu]})
-
-	#print (readport)
-	#print (ponNumOnu)
 
 port_down = []
 for i in range(1,len(result)+1):
 	if int(result[i][0]) == 0 and int(result[i][1]) > 1:
 		port_down.append(i)
-
-#print (result)
-#print (port_down)
 
 if len(port_down) >= 1:
 	print ("PON "+str(port_down)+" Down")
@@ -80,6 +66,3 @@ if not port_down:
 	else:
 		print ("PON Slot is Up")
 		sys.exit(0)
-
-
-##################################3
